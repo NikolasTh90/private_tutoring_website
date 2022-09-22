e7e6897c5a041cacdd4ce048e7511d3f6070d34e
apapai05 <82218237+apapai05@users.noreply.github.com>
2022-09-22 09:00:20 +0300

    Read more button

--- epl343/BookingSystem/static/js/read_more.js
+++ epl343/BookingSystem/static/js/read_more.js
@@ -8,8 +8,8 @@ parentContainer.addEventListener('click', event => {
     const currentText = event.target.parentNode.querySelector('.read-more-text')
     currentText.classList.toggle('read-more-text--show')
 
-    current.textContent = current.textContent.includes('Read More...') ? "Read Less... " : "Read More... "
-    if (current.textContent.includes('Read More...'))
+    current.textContent = current.textContent.includes('Read More') ? "Read Less" : "Read More"
+    if (current.textContent.includes('Read More'))
         currentText.style.display = 'none'
     else
         currentText.style.display = 'inherit'
