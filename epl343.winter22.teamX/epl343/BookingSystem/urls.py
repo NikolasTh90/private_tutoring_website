a0064e90789bd6439ed49859ce2195c5d2904fdc
apapai05 <82218237+apapai05@users.noreply.github.com>
2022-09-28 19:12:52 +0300

    contact.html

---
+++ epl343.winter22.teamX/epl343/BookingSystem/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+
+app_name = "bs"
+
+urlpatterns = [
+    # No additional argument -> not something at the end of the route
+    # What will be displayed is handle by views.index()
+    # Name is useful for finding this particural url pattern
+    path('', views.index, name='index'),
+    path('index/', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contacts, name='contacts'),
+    path('book/', views.booking, name='booking'),
+    path('login/', views.login, name='login'),
+    path('terms/', views.tc, name='tc'),
+
+]
