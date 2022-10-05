85a19afdff4fa7feca8abedaa272fc91207c8d10
NikolasTh90 <Nikolasth90@gmail.com>
2022-10-05 18:01:23 +0300

    working on booking

--- epl343/BookingSystem/views.py
+++ epl343/BookingSystem/views.py
@@ -1,3 +1,4 @@
+from .forms import CustomerUpdateForm
 from urllib import request
 from .forms import LoginForm
 from django.contrib.auth import authenticate, login
@@ -10,27 +11,29 @@ from django.contrib import messages
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
-from .models import MyUser
+from .models import *
+from .checks import *
+
+import datetime
+
 
 User = get_user_model()
 
+
 def index(request):
     template = loader.get_template('index.html')
-    return HttpResponse(template.render({'site' : 'Home'}, request))
+    return HttpResponse(template.render({'site': 'Home'}, request))
+
 
 def about(request):
     template = loader.get_template('about.html')
-    return HttpResponse(template.render({'site' : 'About'}, request))
+    return HttpResponse(template.render({'site': 'About'}, request))
+
 
 def contacts(request):
     template = loader.get_template('contacts.html')
-    return HttpResponse(template.render({'site' : 'Contacts'}, request))
+    return HttpResponse(template.render({'site': 'Contacts'}, request))
 
-
-
-def booking(request):
-    template = loader.get_template('index.html')
-    return HttpResponse(template.render({'site' : 'Booking'}, request))
 
 def login1(request):
     if request.user.is_authenticated:
@@ -60,9 +63,11 @@ def login1(request):
         return HttpResponse(template.render(
             {"signinform": AuthenticationForm()}, request))
 
+
 def tc(request):
     template = loader.get_template('terms.html')
-    return HttpResponse(template.render({'site' : 'tc'}, request))
+    return HttpResponse(template.render({'site': 'tc'}, request))
+
 
 def signup(request):
     if request.method == "POST":
@@ -81,16 +86,74 @@ def signup(request):
         context = {"signupform": NewUserForm()}
         return HttpResponse(template.render(context, request))
 
-from .forms import CustomerUpdateForm
 
 @login_required(login_url='bs:login')
 def dashboard(request):
     client = MyUser.objects.filter(email=request.user.email).first()
     customer_update_form = CustomerUpdateForm(instance=client)
-    template =loader.get_template('customer/profile_cust.html')
+    template = loader.get_template('customer/profile_cust.html')
     context = {
             'customer_update_form': customer_update_form,
             'cust': client,
             'age': 15
         }
-    return HttpResponse(template.render(context,request))
+    return HttpResponse(template.render(context, request))
+
+
+def booking(request, findNextBest=True, target_date=None):
+    template = loader.get_template('index.html')
+    if request.method == 'POST':
+        if findNextBest:
+            requested_date = datetime.datetime.strptime(request.POST['theDate'], '%Y-%m-%dT%H:%M')  # convert the date to datetime object
+
+        else:
+			# requested_date = datetime.datetime.strptime(specific_date_time_string, '%Y-%m-%dT%H:%M')#convert the date to datetime object
+            requested_date = target_date
+		#########################################################################################################################
+        if (is_day_off(requested_date)):  # simenei oti iparxi ekseresi tin simerini imera
+            if not findNextBest:
+                return False
+            messages.error(request, _('Pote den dulevume tetoies imeres.'))
+
+        if (argies(requested_date.month, requested_date.day)):
+            if not findNextBest:
+                return False
+            messages.error(request, _('Tin sigekrimeni imera eimaste kleistoi panta.'))
+
+        if (kleistoi_mines(requested_date.month)):
+            if not findNextBest:
+                return False
+        messages.error(request, _('Ton sigekrimeno mina eimaste kleistoi.'))
+
+        # if (daysExceptions(requested_date, request)):
+        #     if not findNextBest:
+        #         return False
+        #     messages.error(request, _('eimaste kleistoi tin sigkekrimeni periodo.'))
+
+        if (timeExceptions(requested_date, request)):
+            if not findNextBest:
+                return False
+            messages.error(request, _('kleinume pio grigora aftin tin imera.'))
+        if (not orarioHmeras(requested_date, request)):
+            if not findNextBest:
+            return False
+        messages.error(request, _(
+			    'simfona me to orario mas aftin tin imera imaste klistoi.'))
+
+        if (hasCollision(requested_date, 60)):
+            if not findNextBest:
+                return False
+            messages.error(request, _('iparxi alo rantevou.'))
+
+        hasColission(2022, requested_date.month, requested_date.day,
+		             requested_date, 60, request)
+
+
+    else:
+        form = AppointmentForm()
+        return render(request, 'applicationForReg/book.html', {'form': form})
+
+
+
+
+    return HttpResponse(template.render({'site' : 'Booking'}, request))
