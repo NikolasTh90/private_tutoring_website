d5f0b1c34da5686af2c61df8cbcdfdc5c5964ea1
NikolasTh90 <Nikolasth90@gmail.com>
2022-10-05 16:07:35 +0300

    trying to fix csrf

--- epl343/epl343/settings.py
+++ epl343/epl343/settings.py
@@ -53,6 +53,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CSRF_TRUSTED_ORIGINS = ['https://*', 'https://*'] # change this, vanurable to attacks
+
+
 ROOT_URLCONF = 'epl343.urls'
 
 TEMPLATES = [
