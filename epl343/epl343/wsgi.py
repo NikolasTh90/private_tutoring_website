21232f4bbc15ff11d7ae9bd6fac429e044592f7d
giannisp01 <gpanis01@ucy.ac.cy>
2022-10-14 17:10:26 +0300

    Fixed wsgi to work with Apache2

--- epl343/epl343/wsgi.py
+++ epl343/epl343/wsgi.py
@@ -7,9 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.append('/home/EPL343_PROJECT/epl343.winter22.teamX')
+sys.path.append('/home/EPL343_PROJECT/epl343.winter22.teamX/epl343')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'epl343.settings')
 
