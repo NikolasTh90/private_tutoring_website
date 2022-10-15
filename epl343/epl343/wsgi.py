ac9f1e50a1b971d36b6e2c559dfc0af6a8d7d4c8
giannisp01 <gpanis01@ucy.ac.cy>
2022-10-15 19:42:05 +0300

    Fix

--- epl343/epl343/wsgi.py
+++ epl343/epl343/wsgi.py
@@ -13,7 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 sys.path.append('/home/ubuntu/epl343.winter22.teamX/epl343')
 sys.path.append('/home/ubuntu/epl343.winter22.teamX/epl343/epl343')
-sys.path.append('/home/ubuntu/epl343.winter22.teamX/epl343/BookingSystem')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'epl343.settings')
 
