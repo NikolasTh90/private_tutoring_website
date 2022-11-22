4023a28c6d3516aa68055b9d43e7787ec66aecf0
NikolasTh90 <Nikolasth90@gmail.com>
2022-11-22 22:22:01 +0200

    urls merge manually

--- epl343/BookingSystem/urls.py
+++ epl343/BookingSystem/urls.py
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-from django.urls import path
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
-
-app_name = "bs"
-
-urlpatterns = [
-    # No additional argument -> not something at the end of the route
-    # What will be displayed is handle by views.index()
-    # Name is useful for finding this particural url pattern
-    path('', views.index, name='index'),
-    path('index/', views.index, name='index'),
-    path('home/', views.index, name='index'),
-    path('about/', views.about, name='about'),
-    path('contact/', views.contacts, name='contacts'),
-    path('booking/', views.booking, name='booking'),
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login1, name='login'),
-    path('login-signup/', views.login1, name='login'),
-    path('logout/', views.logout1, name='logout1'),
-    path('terms/', views.tc, name='tc'),
-    path('dashboard/',views.dashboard,name='dashboard'),
-    path('teaching-experience/', views.teaching, name='teaching'),
-    path('testimonials/', views.testimonials, name='testimonials'),
-    path('feedback/', views.feedback, name='feedback'),
-    path('temp/',views.temp,name='temp'),
-    path('myappointments/', views.myappointments, name = 'myappointments')
-
-=======
 from django.urls import path
 from . import views
 from django.conf import settings
@@ -62,8 +31,10 @@ urlpatterns = [
     path('addAllLearningMaterial/<int:id>', views.addUserToLearningMaterial, name='addUserToLearningMateriald'),
     path('getalllearningmaterialadmin/', views.getAllLearningMaterial, name='getallmaterial'),
     path('addTestimonial/', views.addTestimonial, name='addTestimonial'),
+    path('temp/',views.temp,name='temp'),
+    path('myappointments/', views.myappointments, name = 'myappointments')
 
 
 
->>>>>>> 54e850644a3b9425c4538760e279d2165bd09632
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
