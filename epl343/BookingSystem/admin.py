47bf2057f20f2d7aca4184cc986547521e1fecda
giannisp01 <gpanis01@ucy.ac.cy>
2022-11-23 15:10:59 +0200

    Revert to Revert merge branch cd38fe4

--- epl343/BookingSystem/admin.py
+++ epl343/BookingSystem/admin.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from operator import is_
 from turtle import onclick
 from django.contrib import admin
@@ -59,65 +58,3 @@ admin.site.register(Schedule)
 admin.site.register(Offs)
 
 
-=======
-from operator import is_
-from turtle import onclick
-from django.contrib import admin
-from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import ReadOnlyPasswordHashField
-from django.contrib.auth.models import Group
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
-from django import forms
-
-from .forms import NewUserForm,UserChangeForm
-
-from .models import *
-
-User = get_user_model()
-
-
-# Remove Group Model from admin. We're not using it.
-admin.site.unregister(Group)
-
-class UserAdmin(BaseUserAdmin):
-    # The forms to add and change user instances
-    form = UserChangeForm
-    add_form = NewUserForm
-
-    # The fields to be used in displaying the User model.
-    # These override the definitions on the base UserAdmin
-    # that reference specific fields on auth.User.
-    list_display = ['email', 'first_name', 'last_name', 'admin', 'staff']
-    list_filter = ['admin', 'staff']
-    # For change/update user
-    fieldsets = (
-        ('Credentials', {'fields': ('email','password')}),
-        ('Personal info', {'fields': ('first_name', 'last_name','preferred_loc', 'year', 'pay', 'is_student','profilePic','school','yearofStudy')}),
-        ('Permissions', {'fields': ('admin','staff',)}),
-    )
-    # add_fieldsets is not a standard ModelAdmin attribute. UserAdmin
-    # overrides get_fieldsets to use this attribute when creating a user.
-    # For add/new/create user
-    add_fieldsets = (
-        (None, {
-            'classes': ('wide',),
-            'fields': ('email', 'password1', 'password2', 'first_name', 'last_name', 'is_student', 'preferred_loc', 'year', 'pay','profilePic','school','yearofStudy')}
-        ),
-    )
-    search_fields = ['last_name']
-    ordering = ['email']
-    filter_horizontal = ()
-
-
-admin.site.register(User, UserAdmin)
-admin.site.register(Testimonial)
-admin.site.register(Teaching_experience)
-admin.site.register(PhotoSection)
-admin.site.register(Photo)
-admin.site.register(Appointment)
-admin.site.register(Schedule)
-admin.site.register(Offs)
-
-
->>>>>>> 54e850644a3b9425c4538760e279d2165bd09632
