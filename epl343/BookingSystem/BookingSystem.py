f163bbe19a1f3619461aa125802048d15ebde827
apapai05 <82218237+apapai05@users.noreply.github.com>
2022-11-25 00:03:32 +0200

    changes

--- epl343/BookingSystem/BookingSystem.py
+++ epl343/BookingSystem/BookingSystem.py
@@ -2,6 +2,7 @@ import datetime
 from .models import Schedule, Offs, Appointment, MyUser
 from django.db.models import Q
 import django.utils.timezone as timezone
+from .smtp_service import send_booking
 break_time = datetime.timedelta(minutes = 10)
 allowed_days_before_appointment = datetime.timedelta(days = 3)
 
@@ -14,7 +15,8 @@ def main(post_request):
         #user = MyUser.objects.only(email).filter(email=post_request['user_email'])
         #TODO user should be a MyUser instance, can we get this from sessions?
         Appointment.objects.create(user = post_request['user_email'], description = post_request['description'], duration = post_request['requested_duration'], start_dateTime = post_request['requested_dateTime']) 
-        #TODO send email notification
+        #end email notification
+        send_booking("pending")
 
     else:
         recommendations = [recommend_next_appointment(post_request['requested_dateTime'], post_request['requested_duration']),
